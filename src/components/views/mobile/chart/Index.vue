4e4d49734b290f7f8afc29126bb6b85377f8e41a
hanhoon <hanhoon12@naver.com>
2025-06-26 14:26:52 +0900

    Add chart.js

--- src/components/views/mobile/chart/Index.vue
+++ src/components/views/mobile/chart/Index.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import { numberFormat } from '@/util'
-import { Column, DataTable, Tab, TabList, TabPanel, TabPanels, Tabs } from 'primevue'
+import { Column, DataTable, Divider, Tab, TabList, TabPanel, TabPanels, Tabs } from 'primevue'
+import Chart from 'primevue/chart'
 
 const text = {
 	count: '총계',
@@ -8,6 +9,51 @@ const text = {
 }
 
 const testData = [
+	{
+		cpn: '새회사',
+		cpc: 1234,
+		cpd: '2025-06-26',
+	},
+	{
+		cpn: '새회사',
+		cpc: 1234,
+		cpd: '2025-06-26',
+	},
+	{
+		cpn: '새회사',
+		cpc: 1234,
+		cpd: '2025-06-26',
+	},
+	{
+		cpn: '새회사',
+		cpc: 1234,
+		cpd: '2025-06-26',
+	},
+	{
+		cpn: '새회사',
+		cpc: 1234,
+		cpd: '2025-06-26',
+	},
+	{
+		cpn: '새회사',
+		cpc: 1234,
+		cpd: '2025-06-26',
+	},
+	{
+		cpn: '새회사',
+		cpc: 1234,
+		cpd: '2025-06-26',
+	},
+	{
+		cpn: '새회사',
+		cpc: 1234,
+		cpd: '2025-06-26',
+	},
+	{
+		cpn: '새회사',
+		cpc: 1234,
+		cpd: '2025-06-26',
+	},
 	{
 		cpn: '새회사',
 		cpc: 1234,
@@ -29,13 +75,16 @@ const testData = [
 					<p>{{ text.date }}: 2025-06-26</p>
 				</TabPanel>
 				<TabPanel value="1">
-					<DataTable :value="testData">
+					<DataTable :value="testData" scrollable scroll-height="300px">
 						<Column field="cpn" header="회사" />
 						<Column :header="text.count">
 							<template #body="{ data }">{{ numberFormat(data.cpc) }}</template>
 						</Column>
 						<Column field="cpd" :header="text.date" />
 					</DataTable>
+					<Divider />
+					Top 3
+					<Chart type="pie" :data="{ labels: ['A', 'B', 'C'], datasets: [{ data: [50, 30, 20] }] }" />
 				</TabPanel>
 			</TabPanels>
 		</Tabs>
