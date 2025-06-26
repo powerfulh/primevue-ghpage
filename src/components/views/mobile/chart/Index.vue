692597db470cf7d5b3a46094fa3403c189c60d1f
hanhoon <hanhoon12@naver.com>
2025-06-26 13:10:49 +0900

    Add numeral

--- src/components/views/mobile/chart/Index.vue
+++ src/components/views/mobile/chart/Index.vue
@@ -1,4 +1,5 @@
 <script setup lang="ts">
+import { numberFormat } from '@/util';
 import { Tab, TabList, TabPanel, TabPanels, Tabs } from 'primevue'
 </script>
 
@@ -10,7 +11,10 @@ import { Tab, TabList, TabPanel, TabPanels, Tabs } from 'primevue'
 				<Tab value="1">회사별</Tab>
 			</TabList>
 			<TabPanels>
-				<TabPanel value="0"> t </TabPanel>
+				<TabPanel value="0">
+					<p>총계: {{ numberFormat(1234) }}</p>
+					<p>최근 수집일: 2025-06-26</p>
+				</TabPanel>
 				<TabPanel value="1"> t </TabPanel>
 			</TabPanels>
 		</Tabs>
