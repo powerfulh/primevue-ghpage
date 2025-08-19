723a39b65c93cbae44c3f397b197a68daebb0afa
hanhoon <hanhoon12@naver.com>
2025-08-19 14:54:40 +0900

    Tile direction

--- src/components/views/mobile/game/TileDetail.vue
+++ src/components/views/mobile/game/TileDetail.vue
@@ -3,6 +3,7 @@ import { getElementNameList, Tile } from '@/util/heat'
 
 const temper = defineModel('temper')
 const g = defineModel('g')
+const dir = defineModel('dir')
 defineProps<{
 	tileList: Array<Tile>
 	currentCell: number
@@ -31,4 +32,32 @@ const emit = defineEmits(['change-el', 'change-activate'])
 	Temper: <input v-model="temper" type="number" :disabled="playing" />
 	<br />
 	Mass: <input v-model="g" type="number" :disabled="playing" />
+	<div class="direction">
+		<div style="position: absolute; font-size: large; left: var(--p-popover-content-padding)">Direction</div>
+		<div></div>
+		<div :class="{ active: dir == 'u' }" @click="dir = 'u'">⬆</div>
+		<div></div>
+		<div :class="{ active: dir == 'l' }" @click="dir = 'l'">⬅</div>
+		<div :class="{ active: !dir }" @click="dir = ''"></div>
+		<div :class="{ active: dir == 'r' }" @click="dir = 'r'">➡</div>
+		<div></div>
+		<div :class="{ active: dir == 'd' }" @click="dir = 'd'">⬇</div>
+		<div></div>
+	</div>
 </template>
+
+<style lang="scss" scoped>
+.direction {
+	width: 3em;
+	display: flex;
+	flex-wrap: wrap;
+	margin: auto;
+	font-size: xx-large;
+	> div {
+		width: 1em;
+	}
+	> .active {
+		background-color: blue;
+	}
+}
+</style>
