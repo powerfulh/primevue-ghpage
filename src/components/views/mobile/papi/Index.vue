0ec05f09737f773da05be0727388213d054f9148
hanhoon <hanhoon12@naver.com>
2025-08-25 15:03:12 +0900

    Add item

--- src/components/views/mobile/papi/Index.vue
+++ src/components/views/mobile/papi/Index.vue
@@ -1,12 +1,16 @@
 <script setup lang="ts">
-import { Card } from 'primevue'
+import { Button, Card, Chip, InputText, useToast } from 'primevue'
 import LoginDialog from '../../LoginDialog.vue'
 import { injectApi } from 'powerful-api-vue3'
-import { ref } from 'vue'
+import { computed, ref } from 'vue'
 
 const api = injectApi()
+const toast = useToast()
 
 const model = ref({})
+const newItemKey = 'Define me'
+
+const addable = computed(() => model.value[newItemKey] ?? true)
 
 function init() {
 	api.load('getPapi')
@@ -23,14 +27,37 @@ function init() {
 		})
 		.fire()
 }
+function onClickChip(k: string) {
+	const nk = prompt('What is new keyword:')
+	if (nk == null || nk.trim() == '') return
+	if (model.value[nk]) {
+		toast.add({ detail: `${nk} is already defined`, life: 2000, severity: 'error' })
+		return
+	}
+	model.value[nk] = model.value[k]
+	delete model.value[k]
+}
 </script>
 
 <template>
 	<main>
 		<Card>
-			<template #content>a</template>
+			<template #content>
+				<div v-for="(item, i) in Object.keys(model)" :key="i" style="text-align: center">
+					<Chip :label="item" class="k" @click="onClickChip(item)" /> : <InputText class="v" />
+				</div>
+				<br />
+				<Button icon="pi pi-plus" :disabled="addable != true" @click="model[newItemKey] = ''" />
+			</template>
 		</Card>
 
 		<LoginDialog @login="init" />
 	</main>
 </template>
+
+<style lang="scss" scoped>
+.k,
+.v {
+	width: 45%;
+}
+</style>
