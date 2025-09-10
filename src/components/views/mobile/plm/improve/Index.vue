c299fb28cd5956d7e99902a6bed96073fe155234
hanhoon <hanhoon12@naver.com>
2025-09-10 16:10:41 +0900

    Sample

--- src/components/views/mobile/plm/improve/Index.vue
+++ src/components/views/mobile/plm/improve/Index.vue
@@ -1,12 +1,18 @@
 <script setup lang="ts">
 import { Card } from 'primevue'
+import JsonPretty from 'vue-json-pretty'
+import 'vue-json-pretty/lib/styles.css'
+
+const t = { list: [{ n: 0, word: 'word' }, { n: 0 }], point: 0 }
 </script>
 
 <template>
 	<main>
 		<Card>
 			<template #title>Src</template>
-			<template #content>{{ { list: [], point: 0 } }}</template>
+			<template #content>
+				<JsonPretty :data="t" />
+			</template>
 		</Card>
 	</main>
 </template>
