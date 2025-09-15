0b91454ce456033c9602bb2118c5d725fa312a87
hanhoon <hanhoon12@naver.com>
2025-09-15 14:15:42 +0900

    Updates

---
+++ assets/Index-COMrANnx.js
@@ -0,0 +1,98 @@
+import{B as K,s as G,j as I,o as $,q as M,n as N,t as D,y as z,W as A,z as m,v as e,k as B,l as W,C as p,p as i,w as o,X as j,Y as x,$ as y,x as c,u as T}from"./index-DHkf7L0h.js";import{_ as q}from"./LoginDialog.vue_vue_type_script_setup_true_lang-BsM7_jU8.js";import{_ as E}from"./LearnItem.vue_vue_type_script_setup_true_lang-BjhFCE3G.js";import{s as L,a as f}from"./index-DySscg1J.js";import{s as V}from"./index-AhHPlCUn.js";import"./index-CPrbWFvY.js";var O=`
+    .p-inputgroup,
+    .p-inputgroup .p-iconfield,
+    .p-inputgroup .p-floatlabel,
+    .p-inputgroup .p-iftalabel {
+        display: flex;
+        align-items: stretch;
+        width: 100%;
+    }
+
+    .p-inputgroup .p-inputtext,
+    .p-inputgroup .p-inputwrapper {
+        flex: 1 1 auto;
+        width: 1%;
+    }
+
+    .p-inputgroupaddon {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        padding: dt('inputgroup.addon.padding');
+        background: dt('inputgroup.addon.background');
+        color: dt('inputgroup.addon.color');
+        border-block-start: 1px solid dt('inputgroup.addon.border.color');
+        border-block-end: 1px solid dt('inputgroup.addon.border.color');
+        min-width: dt('inputgroup.addon.min.width');
+    }
+
+    .p-inputgroupaddon:first-child,
+    .p-inputgroupaddon + .p-inputgroupaddon {
+        border-inline-start: 1px solid dt('inputgroup.addon.border.color');
+    }
+
+    .p-inputgroupaddon:last-child {
+        border-inline-end: 1px solid dt('inputgroup.addon.border.color');
+    }
+
+    .p-inputgroupaddon:has(.p-button) {
+        padding: 0;
+        overflow: hidden;
+    }
+
+    .p-inputgroupaddon .p-button {
+        border-radius: 0;
+    }
+
+    .p-inputgroup > .p-component,
+    .p-inputgroup > .p-inputwrapper > .p-component,
+    .p-inputgroup > .p-iconfield > .p-component,
+    .p-inputgroup > .p-floatlabel > .p-component,
+    .p-inputgroup > .p-floatlabel > .p-inputwrapper > .p-component,
+    .p-inputgroup > .p-iftalabel > .p-component,
+    .p-inputgroup > .p-iftalabel > .p-inputwrapper > .p-component {
+        border-radius: 0;
+        margin: 0;
+    }
+
+    .p-inputgroupaddon:first-child,
+    .p-inputgroup > .p-component:first-child,
+    .p-inputgroup > .p-inputwrapper:first-child > .p-component,
+    .p-inputgroup > .p-iconfield:first-child > .p-component,
+    .p-inputgroup > .p-floatlabel:first-child > .p-component,
+    .p-inputgroup > .p-floatlabel:first-child > .p-inputwrapper > .p-component,
+    .p-inputgroup > .p-iftalabel:first-child > .p-component,
+    .p-inputgroup > .p-iftalabel:first-child > .p-inputwrapper > .p-component {
+        border-start-start-radius: dt('inputgroup.addon.border.radius');
+        border-end-start-radius: dt('inputgroup.addon.border.radius');
+    }
+
+    .p-inputgroupaddon:last-child,
+    .p-inputgroup > .p-component:last-child,
+    .p-inputgroup > .p-inputwrapper:last-child > .p-component,
+    .p-inputgroup > .p-iconfield:last-child > .p-component,
+    .p-inputgroup > .p-floatlabel:last-child > .p-component,
+    .p-inputgroup > .p-floatlabel:last-child > .p-inputwrapper > .p-component,
+    .p-inputgroup > .p-iftalabel:last-child > .p-component,
+    .p-inputgroup > .p-iftalabel:last-child > .p-inputwrapper > .p-component {
+        border-start-end-radius: dt('inputgroup.addon.border.radius');
+        border-end-end-radius: dt('inputgroup.addon.border.radius');
+    }
+
+    .p-inputgroup .p-component:focus,
+    .p-inputgroup .p-component.p-focus,
+    .p-inputgroup .p-inputwrapper-focus,
+    .p-inputgroup .p-component:focus ~ label,
+    .p-inputgroup .p-component.p-focus ~ label,
+    .p-inputgroup .p-inputwrapper-focus ~ label {
+        z-index: 1;
+    }
+
+    .p-inputgroup > .p-button:not(.p-button-icon-only) {
+        width: auto;
+    }
+
+    .p-inputgroup .p-iconfield + .p-iconfield .p-inputtext {
+        border-inline-start: 0;
+    }
+`,X={root:"p-inputgroup"},Y=K.extend({name:"inputgroup",style:O,classes:X}),F={name:"BaseInputGroup",extends:G,style:Y,provide:function(){return{$pcInputGroup:this,$parentInstance:this}}},g={name:"InputGroup",extends:F,inheritAttrs:!1};function H(v,w,b,l,k,a){return $(),I("div",N({class:v.cx("root")},v.ptmi("root")),[M(v.$slots,"default")],16)}g.render=H;const J={style:{"text-align":"center"}},Q={style:{"text-align":"center"}},tn=D({__name:"Index",setup(v){const w=z(),b=A(),l=m({word:"",type:"무엇",memo:""}),k=["무엇","결합","어미","0","조사","1","기호","대명사","접속","감탄사"],a=m(""),u=m({word:null,leftword:null,rightword:null}),h=m({n:null}),s=m(null),d=m({leftword:null,rightword:null});function C(){w.load("postWord").setParameter(l).setWhenSuccess(()=>{b.add({detail:"Post ✔",life:2e3}),a.value=[l.value.word,l.value.type,l.value.memo].filter(r=>r).join(),s.value=null,l.value.word=l.value.memo=""}).fire({credentials:!0})}function P(){w.load("postCompound").setParameter(u).setWhenSuccess(r=>{b.add({detail:"Post ✔",life:2e3});const n=r.at(-1);a.value=`${n.cw} = ${n.lw} + ${n.rw}`,s.value=null,u.value.word=u.value.leftword=u.value.rightword=null}).fire({credentials:!0})}function U(){w.load("deleteLearn").setParameter(h).setWhenSuccess(r=>{b.add({detail:"Delete ✔",life:2e3}),a.value="",s.value=r,h.value.n=null}).fire({credentials:!0})}function S(){w.load("postContext").setParameter(d).setWhenSuccess(r=>{b.add({detail:"Post ✔",life:2e3}),a.value=`${r.lw} + ${r.rw} 🔼 ${r.cnt}`,s.value=null,d.value.leftword=d.value.rightword=null}).fire({credentials:!0})}return(r,n)=>($(),I("main",null,[e(o(V),null,{title:p(()=>n[10]||(n[10]=[c("단어 등록")])),content:p(()=>[i("form",null,[e(o(g),null,{default:p(()=>[e(o(j),{modelValue:l.value.word,"onUpdate:modelValue":n[0]||(n[0]=t=>l.value.word=t),placeholder:"Word"},null,8,["modelValue"]),e(o(L),{modelValue:l.value.type,"onUpdate:modelValue":n[1]||(n[1]=t=>l.value.type=t),options:k},null,8,["modelValue"])]),_:1}),e(o(g),null,{default:p(()=>[e(o(j),{modelValue:l.value.memo,"onUpdate:modelValue":n[2]||(n[2]=t=>l.value.memo=t),placeholder:"Memo",maxlength:"29",onKeypress:x(C,["enter"])},null,8,["modelValue"])]),_:1})]),n[11]||(n[11]=i("hr",null,null,-1)),i("footer",J,[e(o(y),{icon:"pi pi-check",onClick:C})])]),_:1}),e(o(V),null,{title:p(()=>n[12]||(n[12]=[c("결합 등록")])),content:p(()=>[i("form",null,[e(o(g),null,{default:p(()=>[e(o(f),{modelValue:u.value.word,"onUpdate:modelValue":n[3]||(n[3]=t=>u.value.word=t),placeholder:"🆎🆔"},null,8,["modelValue"]),e(o(f),{modelValue:u.value.leftword,"onUpdate:modelValue":n[4]||(n[4]=t=>u.value.leftword=t),placeholder:"⬅🆔"},null,8,["modelValue"]),e(o(f),{modelValue:u.value.rightword,"onUpdate:modelValue":n[5]||(n[5]=t=>u.value.rightword=t),placeholder:"➡🆔",onKeypress:x(P,["enter"])},null,8,["modelValue"])]),_:1})]),n[13]||(n[13]=i("hr",null,null,-1)),i("footer",Q,[e(o(y),{icon:"pi pi-check",onClick:P})])]),_:1}),e(o(V),null,{title:p(()=>[n[14]||(n[14]=c("문맥 조정 ")),i("button",{onClick:n[6]||(n[6]=t=>d.value.leftword=2903)},"Opener")]),content:p(()=>[i("form",null,[e(o(g),null,{default:p(()=>[e(o(f),{modelValue:d.value.leftword,"onUpdate:modelValue":n[7]||(n[7]=t=>d.value.leftword=t),placeholder:"⬅🆔"},null,8,["modelValue"]),e(o(f),{modelValue:d.value.rightword,"onUpdate:modelValue":n[8]||(n[8]=t=>d.value.rightword=t),placeholder:"➡🆔",onKeypress:x(S,["enter"])},null,8,["modelValue"]),e(o(y),{icon:"pi pi-check",onClick:S})]),_:1})])]),_:1}),a.value?($(),B(o(V),{key:0},{title:p(()=>n[15]||(n[15]=[c("방금 등록됨✔")])),content:p(()=>[c(T(a.value),1)]),_:1})):W("",!0),e(o(V),null,{title:p(()=>n[16]||(n[16]=[c("학습 취소")])),content:p(()=>[e(o(g),null,{default:p(()=>[e(o(f),{modelValue:h.value.n,"onUpdate:modelValue":n[9]||(n[9]=t=>h.value.n=t),placeholder:"🆎🆔",onKeypress:x(U,["enter"])},null,8,["modelValue"]),e(o(y),{icon:"pi pi-check",onClick:U})]),_:1})]),_:1}),s.value?($(),B(E,{key:1,item:s.value},null,8,["item"])):W("",!0),e(q)]))}});export{tn as default};
