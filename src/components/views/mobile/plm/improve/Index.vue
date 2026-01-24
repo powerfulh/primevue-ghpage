375adbaaaed655ab19979e93376f4688a794d1ae
powerful-room <hanhoon12@naver.com>
2026-01-24 13:53:36 +0900

    Sentence ui

--- src/components/views/mobile/plm/improve/Index.vue
+++ src/components/views/mobile/plm/improve/Index.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 import { injectApi } from 'powerful-api-vue3'
-import { Button, Card } from 'primevue'
+import { Button, Card, InputGroup, InputNumber, Message } from 'primevue'
 import { ref } from 'vue'
 import JsonPretty from 'vue-json-pretty'
 import 'vue-json-pretty/lib/styles.css'
@@ -8,6 +8,7 @@ import 'vue-json-pretty/lib/styles.css'
 const api = injectApi()
 
 const understand = ref([])
+const sentenceTarget = ref(null as number)
 
 function deactivate(n: number) {
 	api.load('putUnderstandBoxActivation').setParameter(ref({ n })).setWhenSuccess(init).fire()
@@ -32,5 +33,18 @@ init()
 				<Button icon="pi pi-check" @click="deactivate(item.n)" />
 			</template>
 		</Card>
+		<template v-if="understand.length == 0">
+			<Message severity="contrast" icon="pi pi-check" style="margin-top: 4px">이해 대상 없음 > 문장 현황</Message>
+			<Card>
+				<template #title>Sentence content</template>
+				<template #subtitle>N, ymd</template>
+				<template #content>
+					<InputGroup>
+						<InputNumber v-model="sentenceTarget" placeholder="Target n" />
+						<Button icon="pi pi-check" />
+					</InputGroup>
+				</template>
+			</Card>
+		</template>
 	</main>
 </template>
